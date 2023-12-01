cf35cf16eb3291a24d181678c756a7972b6cacc7
manar27112000 <adel0115225@gmail.com>
2023-12-01 21:15:16 +0200

    add game sound

--- Rabbit.java
+++ Rabbit.java
@@ -1,9 +1,12 @@
-
 import com.sun.opengl.util.Animator;
 import java.awt.BorderLayout;
 import javax.media.opengl.GLCanvas;
 import javax.swing.JFrame;
-
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 public class Rabbit extends JFrame {
      private Animator Animation;
@@ -30,7 +33,35 @@ public class Rabbit extends JFrame {
  }
     
     public static void main(String []args){
+        String filepath ="Sound/Run-Amok(chosic.com).wav";
+        PlayMusic(filepath);
+        // JOptionPane.showMessageDialog(null,"press button to stop playing");
+
+
         new Rabbit();
     }
+    private static void PlayMusic(String location) {
+        try
+        {
+            File musicPath = new File(location);
+            if(musicPath.exists())
+            {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip =AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+            }
+            else
+            {
+                System.out.println("Can't find file");
+
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+
+        }
     }
+
+}
 
