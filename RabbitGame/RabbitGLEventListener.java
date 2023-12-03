7bc7365ceb4b4b515fa485a39934309b3ec4d4a2
Abdelfattah Hesham <201927036@std.sci.cu.edu.eg>
2023-12-03 12:58:55 +0200

    add level selection and back button and edit navgite(wafdy)

--- RabbitGame/RabbitGLEventListener.java
+++ RabbitGame/RabbitGLEventListener.java
@@ -26,10 +26,11 @@ public class RabbitGLEventListener extends Component  implements  GLEventListene
 
     int responseOption = 0;
 
-    int maxWidth =1500,maxHeight = 900; // cooredinates of ortho
+      int maxWidth = 1500, maxHeight = 900, level; // cooredinates of ortho
+
 
     String currentScreen = "Home";
-    String textureNames[] = {"Play.png","credits.png","credits.png","credits.png","quit.png","title.png","instructions.png","Menu.png"};
+    String textureNames[] = {"Play.png","credits.png","credits.png","credits.png","quit.png","title.png","Level.png","Hole.png","Hole2.png","Hole3.png","Menu.png"};
     TextureReader.Texture[] texture = new TextureReader.Texture[textureNames.length];
     int textureIndex[] = new int[textureNames.length];
 
@@ -39,17 +40,15 @@ public class RabbitGLEventListener extends Component  implements  GLEventListene
         gl = gld.getGL();
         gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-
         gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
         gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
 
         //number of textures,array to hold the indeces
         gl.glGenTextures(textureNames.length, textureIndex, 0);
 
-
-        for(int i = 0; i < textureNames.length; i++){
+        for (int i = 0; i < textureNames.length; i++) {
             try {
-                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i] , true);
+                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i], true);
                 gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[i]);
 
                 new GLU().gluBuild2DMipmaps(
@@ -60,14 +59,12 @@ public class RabbitGLEventListener extends Component  implements  GLEventListene
                         GL.GL_UNSIGNED_BYTE,
                         texture[i].getPixels() // Imagedata
                 );
-            } catch( IOException e ) {
+            } catch (IOException e) {
                 System.out.println(e);
                 e.printStackTrace();
             }
         }
     }
-
-
 
     @Override
     public void display(GLAutoDrawable glad) {
@@ -77,31 +74,46 @@ public class RabbitGLEventListener extends Component  implements  GLEventListene
         gl.glLoadIdentity();
         switchBetweenScreens();
 
-
     }
 
     // Wafdy:this method for moving from screen to anthor screen (Navigator)
-   public void switchBetweenScreens(){
-        if(currentScreen=="Home") {
-            DrawParentBackground(textureIndex.length - 1);
-            DrawChildBackground(500,450,0.5,5);
-            int marginBetweenbutton = 650;
-            for (int i = 0; i < 5; i++) {
-                DrawChildBackground(200, marginBetweenbutton, 1.2, i);
-                marginBetweenbutton -= 120;
+    public void switchBetweenScreens() {
+        //abdelfattah
+        //draw page
+        switch (currentScreen) {
+            case "Home": {
+                DrawParentBackground(textureIndex.length - 1);
+                DrawChildBackground(500, 450, 0.5, 5);
+                int marginBetweenbutton = 650;
+                for (int i = 0; i < 5; i++) {
+                    DrawChildBackground(200, marginBetweenbutton, 1.2, i);
+                    marginBetweenbutton -= 120;
+                }
+                break;
             }
-        }else if(currentScreen=="Credits"){
-            DrawParentBackground(textureIndex.length-1);
+            case "Play":
+                DrawParentBackground(6);
+                break;
 
+            case "Game":
+                if (level < 4) { //easy
+                    DrawParentBackground(7);
+                } else if (level < 7) {
+                    DrawParentBackground(8);
+                } else  {
+                    DrawParentBackground(9);
+                }
+                break;
+
+            case "Credits":
+                DrawParentBackground(textureIndex.length - 1);
+                break;
+            case "How to play":
+                DrawParentBackground(textureIndex.length - 1);
+                DrawChildBackground(10, 540, 0.5, 6);
+                break;
 
         }
-   else if(currentScreen=="How to play"){
-
-            DrawParentBackground(textureIndex.length-1);
-
-            DrawChildBackground(10,540,0.5,6);
-
-    }
 
     }
 
@@ -123,59 +135,140 @@ public class RabbitGLEventListener extends Component  implements  GLEventListene
     @Override
     public void mouseMoved(MouseEvent e) {
 
-        System.out.println(e.getX()+"  "+e.getY());
-
-
-
+        System.out.println(e.getX() + "  " + e.getY());
 
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        //abdelfattah
+      //action to nvegite
+        switch (currentScreen) {
+            case "Home":
+                if (e.getX() > 45 && e.getX() < 310 && e.getY() > 105 && e.getY() < 185) {
+                    currentScreen = "Play";
+                }
+                break;
+//back button
+            case "Play":
 
+                if (e.getX() > 25 && e.getX() < 150 && e.getY() > 15 && e.getY() < 95) {
+                    currentScreen = "Home";
+                }
+                if (e.getX() > 440 && e.getX() < 630 && e.getY() > 280 && e.getY() < 390) {
+                    currentScreen = "Game";
+                    level = 1;
+                }
+                if (e.getX() > 760 && e.getX() < 925 && e.getY() > 280 && e.getY() < 390) {
+                    currentScreen = "Game";
+                    level = 2;
+                }
+                if (e.getX() > 1025 && e.getX() < 1180 && e.getY() > 280 && e.getY() < 390) {
+                    currentScreen = "Game";
+                    level = 3;
+                }
+                if (e.getX() > 470 && e.getX() < 615 && e.getY() > 440 && e.getY() < 540) {
+                    currentScreen = "Game";
+                    level = 4;
+                }
+                if (e.getX() > 750 && e.getX() < 935 && e.getY() > 430 && e.getY() < 530) {
+                    currentScreen = "Game";
+                    level = 5;
+                }
+                if (e.getX() > 1015 && e.getX() < 1200 && e.getY() > 425 && e.getY() < 525) {
+                    currentScreen = "Game";
+                    level = 6;
+                }
+                if (e.getX() > 450 && e.getX() < 630 && e.getY() > 590 && e.getY() < 690) {
+                    currentScreen = "Game";
+                    level = 7;
+                }
+                if (e.getX() > 735 && e.getX() < 910 && e.getY() > 590 && e.getY() < 690) {
+                    currentScreen = "Game";
+                    level = 8;
+                }
+                if (e.getX() > 1020 && e.getX() < 1120 && e.getY() > 590 && e.getY() < 690) {
+                    currentScreen = "Game";
+                    level = 9;
+                }
+                break;
+//
+            case "Game":
+                if (e.getX() > 25 && e.getX() < 150 && e.getY() > 15 && e.getY() < 95) {
+                    currentScreen = "Play";
+                }
+                break;
         }
-
-
+glc.repaint();
+        //game(1); --> 5 hit 20s
+        //game(2); ---> 5 hits 18s
+        //game(3); ---> 5 hits 16s
+        // game(4) --> 7
+    }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        int minX = 45, maxX = 310;
-        int minY = 105, maxY = 185, Height = maxY - minY;
-        for (int i = 0; i < 5; i++) {
+//        int minX = 45, maxX = 310;
+//        int minY = 105, maxY = 185, Height = maxY - minY;
+//        for (int i = 0; i < 5; i++) {
+//
+//            if (e.getX() > minX && e.getX() < maxX && e.getY() > minY && e.getY() < maxY) {
+//                System.out.println("i = " + i);
+//                scaleButton = 1;
+//                if (i == 4) {
+//                    responseOption = JOptionPane.showConfirmDialog(this, "Are you sure to exit?",
+//                            "exit", JOptionPane.ERROR_MESSAGE);
+//                    if (responseOption == JOptionPane.YES_OPTION) {
+//                        System.exit(0);
+//                    }
+//                } else if (i == 1) {
+//
+//                    currentScreen = "Credits";
+//                    System.out.println(currentScreen);
+//                } else if (i == 2) {
+//
+//                    currentScreen = "How to play";
+//                    System.out.println(currentScreen);
+//                } else if (i == 3) {
+//                    currentScreen = "Diffculty";
+//                    minY = 105;
+//                    maxY = 185;
+//                    for (int j = 0; j < 3; j++) {
+//                        if (e.getX() > minX && e.getX() < maxX && e.getY() > minY && e.getY() < maxY) {
+//
+////                        System.out.println(currentScreen);
+//                            if (j == 0) {
+//
+//                                currentScreen = "Easy";
+//                                System.out.println(currentScreen);
+//                            } else if (j == 1) {
+//
+//                                currentScreen = "Normal";
+//                                System.out.println(currentScreen);
+//
+//                            } else if (j == 2) {
+//
+//                                currentScreen = "Hard";
+//                                System.out.println(currentScreen);
+//
+//                            }
+//                        }
+//                        minY += Height + 55;
+//                        maxY += Height + 55;
+//                    }
+//
+//                }
+//
+//            }
+//
+//            minY += Height + 55;
+//            maxY += Height + 55;
+//        }
 
-
-            if (e.getX() > minX && e.getX() < maxX && e.getY() > minY && e.getY() < maxY) {
-                System.out.println("i = " + i);
-                scaleButton = 1;
-                if(i==4){
-    responseOption = JOptionPane.showConfirmDialog(this, "Are you sure to exit?",
-            "exit", JOptionPane.ERROR_MESSAGE);
-    if (responseOption == JOptionPane.YES_OPTION) {
-        System.exit(0);
-                }
-    }  else if(i==1){
-
-        currentScreen = "Credits";
-        System.out.println(currentScreen);
     }
-                else if(i==2){
 
-                    currentScreen = "How to play";
-                    System.out.println(currentScreen);
-                }
-
-                glc.repaint();
-
-            }
-
-            minY += Height + 55;
-            maxY += Height + 55;
-        }
-
-    }
     @Override
     public void mouseReleased(MouseEvent e) {
-
 
     }
 
@@ -189,9 +282,7 @@ public class RabbitGLEventListener extends Component  implements  GLEventListene
 
     }
 
-
-
-    public void DrawParentBackground(int index){
+    public void DrawParentBackground(int index) {
         gl.glEnable(GL.GL_BLEND);	// Turn Blending On
         gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[index]);
 
@@ -213,12 +304,12 @@ public class RabbitGLEventListener extends Component  implements  GLEventListene
     //created by wafdy
     // this method for draw any background
     //your role is to call this method in the display if you want to draw any background
-    public void DrawChildBackground(int x ,int y,double scale,int index){
+    public void DrawChildBackground(int x, int y, double scale, int index) {
         gl.glEnable(GL.GL_BLEND);	// Turn Blending On
         gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[index]);
         gl.glPushMatrix();
-        gl.glScaled(scale,scale,1);
-        gl.glTranslated(x/(maxWidth/2.0)-0.9,y/(maxHeight/2.0)-0.9,1);
+        gl.glScaled(scale, scale, 1);
+        gl.glTranslated(x / (maxWidth / 2.0) - 0.9, y / (maxHeight / 2.0) - 0.9, 1);
 
         gl.glBegin(GL.GL_QUADS);
         // Front Face
